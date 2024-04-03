fb6d9ac256910d0fab712775f3241c6df9f9d54e
peteryuan <ypf9319@gmail.com>
2024-04-03 22:34:45 +0800

    add loading and preload bg image

--- docs/script/index.js
+++ docs/script/index.js
@@ -1,32 +1,47 @@
 function getBgURL(num) {
-    return `https://peiseka.com/Upload/mihuan/thumb/mihuan${num}.jpg`;
+    return `../asset/mihuan${num}.jpg`;
 }
-
-
+const nums = [1, 2, 3, 4, 5, 6, 7, 18, 22, 55, 78];
+const readyNums = [];
+// 预加载背景图
+(function preloadBgImages(params) {
+    nums.forEach((num) => {
+        const img = new Image();
+        img.src = `${getBgURL(num)}`;
+        img.onload = function () {
+            readyNums.push(num);
+        };
+    });
+})();
+// 定时切换背景图
 function changeBg() {
-    const nums = [1, 2, 3, 4, 5, 6, 7, 18, 24, 55, 78];
     let index = 0;
     return setInterval(() => {
-        if (index === 3) {
+        if (index === readyNums.length - 1) {
             index = 0;
         } else {
             index += 1;
         }
-        document.body.style.backgroundImage = `url(${getBgURL(nums[index])})`;
+        document.body.style.backgroundImage = `url(${getBgURL(
+            readyNums[index],
+        )})`;
     }, 2500);
 }
 let intervalId;
+// 页面不可见的时候清除定时器
 function handleVisibleChange() {
-    if (document.visibilityState === 'visible') {
-        intervalId = changeBg()
+    if (document.visibilityState === "visible") {
+        intervalId = changeBg();
     } else {
         if (intervalId) {
-            clearInterval(intervalId)
+            clearInterval(intervalId);
         }
     }
 }
-handleVisibleChange()
-document.addEventListener('visibilitychange', handleVisibleChange)
+
+handleVisibleChange();
+
+document.addEventListener("visibilitychange", handleVisibleChange);
 
 const HOST = "https://bucket2023.oss-cn-hangzhou.aliyuncs.com/yoki/gallery/";
 
@@ -315,63 +330,75 @@ const works = [
     },
 ];
 let swiper;
+
+// 构造swiper
 function genSwiper() {
     if (swiper) {
-        swiper.destroy()
+        swiper.destroy();
     }
-    const isDesktop = detectDeviceType() === 'Desktop'
-    swiper = new Swiper('.swiper', {
+    const isDesktop = detectDeviceType() === "Desktop";
+    swiper = new Swiper(".swiper", {
         // Optional parameters
-        direction: isDesktop ? 'horizontal' : 'vertical',
+        direction: isDesktop ? "horizontal" : "vertical",
         loop: true,
         autoplay: true,
 
         // If we need pagination
         pagination: {
-            el: '.swiper-pagination',
+            el: ".swiper-pagination",
         },
 
         // Navigation arrows
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
 
         // And if we need scrollbar
         scrollbar: {
-            el: '.swiper-scrollbar',
+            el: ".swiper-scrollbar",
         },
-        effect: 'coverflow',
+        effect: "coverflow",
     });
 }
 
-// 输出 'Mobile' 或 'Desktop'
+// 输出 'Mobile' 或 'Desktop' 判断是不是移动端
 function detectDeviceType() {
     const ua = navigator.userAgent;
-    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
-    return isMobile ? 'Mobile' : 'Desktop';
+    const isMobile =
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            ua,
+        );
+    return isMobile ? "Mobile" : "Desktop";
 }
 
-
+// 渲染作品
 function renderWorks(works) {
-    document.querySelector(".works").innerHTML = ''
+    document.querySelector(".works").innerHTML = "";
     const eles = works.map((work) => genWorkEle(work));
     document.querySelector(".works").append(...eles);
-    genSwiper()
+    genSwiper();
 }
-
+// 构造作品ele
 function genWorkEle(work) {
     const ele = document.createElement("div");
     ele.classList.add("work");
+    ele.classList.add("work-loading");
     ele.classList.add("swiper-slide");
-    ele.setAttribute('data-swiper-autoplay', '2000')
-    ele.style.backgroundImage = `url(${HOST}${encodeURIComponent(work.url)})`;
+    ele.setAttribute("data-swiper-autoplay", "2000");
+    const picURL = `${HOST}${encodeURIComponent(work.url)}`;
+    const img = new Image();
+    img.src = picURL;
+    img.onload = function () {
+        ele.classList.remove("work-loading");
+        ele.style.backgroundImage = `url(${picURL})`;
+    };
     return ele;
 }
 
 renderWorks(works);
 
+// 获取所有Tag值 祛除重复
 function getTags() {
     return Array.from(
         new Set(
@@ -382,26 +409,30 @@ function getTags() {
     );
 }
 
-let selectedTagVals = []
+let selectedTagVals = [];
 
+// Tag点击事件
 function handleTagClicked(e) {
     if (selectedTagVals.includes(e.target.innerText)) {
-        const index = selectedTagVals.findIndex(v => v === e.target.innerText)
-        selectedTagVals.splice(index, 1)
-        e.target.classList.remove('active')
+        const index = selectedTagVals.findIndex(
+            (v) => v === e.target.innerText,
+        );
+        selectedTagVals.splice(index, 1);
+        e.target.classList.remove("active");
     } else {
-        selectedTagVals.push(e.target.innerText)
-        e.target.classList.add('active')
+        selectedTagVals.push(e.target.innerText);
+        e.target.classList.add("active");
     }
-    const _works = getFilteredWorks(selectedTagVals)
-    _works.length ? renderWorks(_works) : renderWorks(works)
-
+    const _works = getFilteredWorks(selectedTagVals);
+    _works.length ? renderWorks(_works) : renderWorks(works);
 }
+// 判断是否有交集
 function hasIntersection(arr1, arr2) {
-    return arr1.some(item => arr2.includes(item));
+    return arr1.some((item) => arr2.includes(item));
 }
+// 过滤作品
 function getFilteredWorks(vals) {
-    return works.filter(item => {
+    return works.filter((item) => {
         if (vals.includes(item.type)) {
             return true;
         }
@@ -411,27 +442,23 @@ function getFilteredWorks(vals) {
         }
 
         return false;
-    })
+    });
 }
-
+// 渲染Tag
 function renderTags(tags) {
     const tagEles = tags.map((tag) => {
         const ele = document.createElement("div");
         ele.classList.add("tag");
         ele.innerText = tag;
-        ele.addEventListener('click', handleTagClicked)
+        ele.addEventListener("click", handleTagClicked);
         return ele;
     });
     document.body.querySelector(".tags").append(...tagEles);
 }
-
-
-
+// 获取类型 祛除重复
 function getTypes() {
     return Array.from(new Set(works.map((v) => v.type)));
 }
-
-
 
 renderTags(getTypes());
 // renderTags(getTags());
